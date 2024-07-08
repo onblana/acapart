e406a3c6e8aaee4e8e206d98a4aa197ebb85d904
onblana <51261847+onblana@users.noreply.github.com>
2024-07-08 18:19:39 +0900

    feat: Navbar에 링크 추가

--- src/components/NavBtn.tsx
+++ src/components/NavBtn.tsx
@@ -1,13 +1,15 @@
 import { Button } from "@/components/ui/Button";
+import Link from "next/link";
 
 interface BtnProps {
   name: string;
+  href: string;
 }
 
-const NavBtn = ({ name }: BtnProps) => {
+const NavBtn = ({ name, href }: BtnProps) => {
   return (
     <Button className="bg-rose-100 hover:bg-rose-200 active:bg-rose-300 w-full">
-      {name}
+      <Link href={href}>{name}</Link>
     </Button>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import NavBtn from "./NavBtn";
 
-const names = ["피치파이프!", "전체보기", "공지", "모집", "자유", "홍보"];
+const buttonInfo = [
+  { name: "피치파이프!", href: "/pitchpipe" },
+  { name: "전체보기", href: "/" },
+  { name: "공지", href: "/notice" },
+  { name: "모집", href: "/recruit" },
+  { name: "자유", href: "/anything" },
+  { name: "홍보", href: "/spotlight" },
+];
 
 const Navbar = () => {
   return (
     <div className="flex bg-rose-100 min-w-screen mb-10 w-full justify-evenly w-full">
-      {names.map((name, i) => (
-        <NavBtn name={name} />
+      {buttonInfo.map((info, i) => (
+        <NavBtn name={info.name} href={info.href} />
       ))}
     </div>
   );
