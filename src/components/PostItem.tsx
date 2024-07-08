7269633ea973c8886cf93e317a77033e88c3e208
onblana <51261847+onblana@users.noreply.github.com>
2024-07-08 18:21:13 +0900

    feat: PostItem 컴포넌트 생성

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,12 @@
+const PostItem = () => {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <span>공지</span>
+      <span>공지사항입니다!</span>
+      <span>작성자</span>
+      <span>07-04</span>
+    </div>
+  );
+};
+
+export default PostItem;
