baad46d72b872186a7a9ac841c822d3b1348ea94
onblana <51261847+onblana@users.noreply.github.com>
2024-07-17 23:34:10 +0900

    feat: GET api로 받아온 게시글 목록 표시 기능 추가

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardHeader,
@@ -6,21 +8,42 @@ import {
   CardContent,
 } from "@/components/ui/Card";
 import PostItem from "./PostItem";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Post from "@/types";
 
-const PostList = () => {
+interface PostListProps {
+  title: string;
+}
+
+const PostList = ({ title }: PostListProps) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/posts")
+      .then((response) => response.data)
+      .then((data) => setPosts(data))
+      .then(() => console.log(posts))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
-    <Card className="mb-20 w-3/4">
-      <CardHeader>
-        <CardTitle>전체 보기</CardTitle>
-        <CardDescription>모든 카테고리의 글을 볼 수 있습니다.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <PostItem />
-        <PostItem />
-        <PostItem />
-        <PostItem />
-      </CardContent>
-    </Card>
+    <div className="mb-20 w-3/4 max-w-3xl">
+      <Card>
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>
+            모든 카테고리의 글을 볼 수 있습니다.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {posts.map((post, i) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
